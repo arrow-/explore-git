fd4ad5e7cc45093bb97d3c0e806cd87631177a11
root <root@hp-compaq-6200.amritanet.edu>
2016-03-10 19:23:43 +0530

    Mod function done.

--- cpp/liquid/src/ml_mod.c
+++ cpp/liquid/src/ml_mod.c
@@ -4,5 +4,8 @@ int ml_mod(int n1, int n2)
 // Compute the remainder when n1 divides n2 (n1 % n2)
 // You cannot use the % operator.
 {
-    return 0;
+ int mod,q;
+	q=n1/n2;
+	mod=n1-(n2*q);
+   return mod;
 }
